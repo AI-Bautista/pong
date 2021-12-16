17fa87ca18c4bde614a10d0fa51c80ab785a7436
Michael Jules Montesa <montesa_3190549@letran.calamba.edu.ph>
2021-12-16 16:54:45 +0800

    Add play Window

--- pong.py
+++ pong.py
@@ -1,2 +1,14 @@
 # This is the beningging of Pong
 # by Bautista and Montesa
+
+import turtle as t
+
+window = t.Screen()
+window.title("Ping Pong Game")
+window.bgcolor("black")
+window.setup(width = 1280, height = 720)
+window.tracer(0)
+# window.exitonclick() #Exit window when screen is clicked
+
+while True:
+    window.update()
