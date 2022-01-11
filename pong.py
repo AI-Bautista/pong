339e4610112f762a4683418e0a3ee41f49c35b17
Michael Jules Montesa <montesa_3190549@letran.calamba.edu.ph>
2022-01-11 15:58:05 +0800

    Scoring Added

--- pong.py
+++ pong.py
@@ -5,28 +5,52 @@ import turtle as t
 
 window = t.Screen()
 window.title("Ping Pong Game")
-window.bgcolor("black")
+window.bgcolor("#1F6420")
 window.setup(width = 1280, height = 720)
 window.tracer(0)
 
 rightbar = t.Turtle()
+rightbar.penup()
 rightbar.color("blue")
 rightbar.shape("square")
 rightbar.shapesize(6,1)
 rightbar.goto(550, 0)
 rightbar.dy = -1
 rightbar.speed(0)
-rightbar.penup()
+
 
 leftbar = t.Turtle()
+leftbar.penup()
 leftbar.color("red")
 leftbar.shape("square")
 leftbar.shapesize(6,1)
 leftbar.goto(-550, 0)
 leftbar.dy = -1
 leftbar.speed(0)
-leftbar.penup()
 
+# middlebar = t.Turtle()
+# middlebar.penup()
+# middlebar.shape("square")
+# middlebar.shapesize(t.window_height(),0.1)
+# middlebar.goto(-1,-1)
+
+#Scoring
+player1Score = 0
+player2Score = 0
+
+
+leftScore = t.Turtle()
+leftScore.penup()
+leftScore.goto(-150,t.window_height()/2.5)
+leftScore.write(player1Score, move=True, font=("Verdana", 40, "bold"))
+
+
+rightScore = t.Turtle()
+rightScore.penup()
+rightScore.goto(105,t.window_height()/2.5)
+rightScore.write(player2Score, move=True, font=("Verdana", 40, "bold"))
+
+#Left Player
 def rightbar_up():
     y = rightbar.ycor()
     y += 25   # add 25 pixels to the y coordinates when going up
@@ -45,6 +69,7 @@ def rightbar_down():
         rightbar.sety(-300)
         rightbar.dy *= -1
 
+#Right Player
 def leftbar_up():
     y = leftbar.ycor()
     y += 25 
